103bf92d26b382ab6dec7a67b7815f9326b0228e
shashi-we <shashi.we@gmail.com>
2014-09-11 22:57:20 +0530

    remove white spaces

--- app/models/config_setting.rb
+++ app/models/config_setting.rb
@@ -1,60 +1,60 @@
 require 'yaml'
 
 class ConfigSetting < ActiveRecord::Base
-	validates_uniqueness_of :key
-	validate :check_type_of_value, on: :update
+  validates_uniqueness_of :key
+  validate :check_type_of_value, on: :update
 
-	def check_type_of_value
-		valid_types = ['String','Float','Fixnum','TrueClass','FalseClass']
-		unless valid_types.include? YAML::load(value).class.name
-			errors.add(:value, "Invalid data type for value!")
-			self.destroy
-			Rails.cache.delete(key)
-		end
-	end
+  def check_type_of_value
+    valid_types = ['String','Float','Fixnum','TrueClass','FalseClass']
+    unless valid_types.include? YAML::load(value).class.name
+    	errors.add(:value, "Invalid data type for value!")
+    	self.destroy
+    	Rails.cache.delete(key)
+    end
+  end
 
-	#get or set a variable with the variable as the called method
-	def self.method_missing(method, *args)
-		method_name = method.to_s
-		super(method, *args)
-	rescue NoMethodError
-		#set a value for a variable
-		if method_name =~ /=$/
-			var_name = method_name.gsub('=', '')
-			self[var_name] = args.first
-		#retrieve a value
-		else
-			self[method_name]
-		end
-	end
+  #get or set a variable with the variable as the called method
+  def self.method_missing(method, *args)
+    method_name = method.to_s
+    super(method, *args)
+  rescue NoMethodError
+    #set a value for a variable
+    if method_name =~ /=$/
+      var_name = method_name.gsub('=', '')
+      self[var_name] = args.first
+    #retrieve a value
+    else
+    	self[method_name]
+    end
+  end
 
-	def self.[](key)
-		key = key.to_s 
-		if cache_val = Rails.cache.read(key)
-			return cache_val
-		end
-		if record = find_by_key(key)
-			value = YAML::load(record.value)
-			Rails.cache.write(key, value)
-			return value
-		end
-	end
+  def self.[](key)
+  	key = key.to_s
+  	if cache_val = Rails.cache.read(key)
+  		return cache_val
+  	end
+  	if record = find_by_key(key)
+  		value = YAML::load(record.value)
+  		Rails.cache.write(key, value)
+  		return value
+    end
+  end
 
-	# save key value pair
-	def self.[]=(key, value)
-		key = key.to_s
-		val = YAML::dump(value)
-		record = find_or_create_by_key(key)
-		record.update_attributes(value: val)
-		Rails.cache.write(key, value)
-	end
+  # save key value pair
+  def self.[]=(key, value)
+  	key = key.to_s
+  	val = YAML::dump(value)
+    record = find_or_create_by_key(key)
+    record.update_attributes(value: val)
+    Rails.cache.write(key, value)
+  end
 
-	# delete setting
-	def self.delete(key)
-		val = send("[]", key)
-		record = find_by_key(key.to_s)
-		record.destroy
-		return val
-	end
+  # delete setting
+  def self.delete(key)
+    val = send("[]", key)
+    record = find_by_key(key.to_s)
+    record.destroy
+    return val
+  end
 
 end
--- spec/models/config_setting_spec.rb
+++ spec/models/config_setting_spec.rb
@@ -3,53 +3,53 @@ require 'spec_helper'
 describe ConfigSetting do
 	
   it "should return nil" do
-		ConfigSetting.new_key.should be_nil
-	end
+    ConfigSetting.new_key.should be_nil
+  end
 
-	it "should save string value" do
-		ConfigSetting.email = 'surendra@email.com'
-		ConfigSetting.count.should == 1
-		ConfigSetting.email.should == 'surendra@email.com'
-	end
+  it "should save string value" do
+    ConfigSetting.email = 'surendra@email.com'
+    ConfigSetting.count.should == 1
+    ConfigSetting.email.should == 'surendra@email.com'
+  end
 
-	it "should save integer value" do
-		ConfigSetting.login_count = 7
-		ConfigSetting.login_count.should == 7
-	end
+  it "should save integer value" do
+    ConfigSetting.login_count = 7
+    ConfigSetting.login_count.should == 7
+  end
 
-	it "should save float value" do
-		ConfigSetting.subscription_charge = 49.99
-		ConfigSetting.subscription_charge.should == 49.99
-	end
+  it "should save float value" do
+    ConfigSetting.subscription_charge = 49.99
+    ConfigSetting.subscription_charge.should == 49.99
+  end
 
-	it "should save boolean value" do
-		ConfigSetting.is_admin = true
-		ConfigSetting.is_admin.should == true
-	end
+  it "should save boolean value" do
+    ConfigSetting.is_admin = true
+    ConfigSetting.is_admin.should == true
+  end
 
-	it "should not save array value" do
-		ConfigSetting.week_days = ['Sun','Mon','Tues','Wed']
-		ConfigSetting.find_by_key('week_days').value.should be_nil
-	end
+  it "should not save array value" do
+  	ConfigSetting.week_days = ['Sun','Mon','Tues','Wed']
+    ConfigSetting.find_by_key('week_days').value.should be_nil
+  end
 
-	it "should update setting" do
-		ConfigSetting.email = 'surendra@email.com'
-		ConfigSetting.count.should == 1
-		ConfigSetting.email = 'surendra-new@email.com'
-		ConfigSetting.count.should == 1
-		ConfigSetting.email.should == 'surendra-new@email.com'
-	end
+  it "should update setting" do
+	  ConfigSetting.email = 'surendra@email.com'
+	  ConfigSetting.count.should == 1
+	  ConfigSetting.email = 'surendra-new@email.com'
+	  ConfigSetting.count.should == 1
+	  ConfigSetting.email.should == 'surendra-new@email.com'
+  end
 	
-	it "should delete" do
-		ConfigSetting.email = 'surendra@email.com'
-		val = ConfigSetting.delete('email')
-		ConfigSetting.count.should == 0
-		val.should == 'surendra@email.com'
-	end
+  it "should delete" do
+    ConfigSetting.email = 'surendra@email.com'
+    val = ConfigSetting.delete('email')
+    ConfigSetting.count.should == 0
+    val.should == 'surendra@email.com'
+  end
 
-	it "should cache" do
-		ConfigSetting.email = 'surendra@email.com'
-		ConfigSetting.should_not_receive(:find_by_key)
-		ConfigSetting.email.should == 'surendra@email.com'
-	end
+  it "should cache" do
+    ConfigSetting.email = 'surendra@email.com'
+    ConfigSetting.should_not_receive(:find_by_key)
+    ConfigSetting.email.should == 'surendra@email.com'
+  end
 end
